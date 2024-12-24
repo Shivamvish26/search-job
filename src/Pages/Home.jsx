46eca4a3aaea55abf934ca4b659fb07c3b22a5cb
Shubham Vishwakarma <127945668+Shivamvish26@users.noreply.github.com>
2024-12-24 18:20:25 +0530

    navbar & hero section done

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,13 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
 import Navbar from "../Components/Navbar";
+import Hero from "../Components/Hero";
 
 export default function Home() {
   return (
     <div>
       <Navbar/>
+      <Hero/>
     </div>
   );
 }
